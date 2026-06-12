cb2c63c190422571a802b27e2084816baa2b3025
a00575272-svg <a00575272@tec.mx>
2026-06-11 19:06:17 -0600

    Implement UserProfile class with JSON serialization
    feat: implementar UserProfile

---
+++ src/UserProfile.cpp
@@ -0,0 +1,50 @@
+#include "UserProfile.h"
+
+UserProfile::UserProfile()
+{
+    userName = "Usuario";
+    preferredModel = "gemma3:4b";
+}
+
+QString UserProfile::getUserName() const
+{
+    return userName;
+}
+
+QString UserProfile::getPreferredModel() const
+{
+    return preferredModel;
+}
+
+void UserProfile::setUserName(const QString &name)
+{
+    userName = name;
+}
+
+void UserProfile::setPreferredModel(const QString &model)
+{
+    preferredModel = model;
+}
+
+QJsonObject UserProfile::toJson() const
+{
+    QJsonObject obj;
+
+    obj["userName"] = userName;
+    obj["preferredModel"] = preferredModel;
+
+    return obj;
+}
+
+UserProfile UserProfile::fromJson(const QJsonObject &obj)
+{
+    UserProfile profile;
+
+    profile.userName =
+        obj["userName"].toString("Usuario");
+
+    profile.preferredModel =
+        obj["preferredModel"].toString("gemma3:4b");
+
+    return profile;
+}
